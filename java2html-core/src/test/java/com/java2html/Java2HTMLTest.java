e7c9d7b78e1d14c81a703a825b91b09ffc76b066
jasons_2000 <jasons_2000@05458adc-50a5-4456-917e-77870aaa2bcd>
2013-05-20 19:37:41 +0000

    ongoing

--- java2html-core/src/test/java/com/java2html/Java2HTMLTest.java
+++ java2html-core/src/test/java/com/java2html/Java2HTMLTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class Java2HTMLTest {
 
     @Test
-    public void testBuildJava2HTML() throws Exception {
+    public void testSampleJava() throws Exception {
         Java2HTML java2HTML = new Java2HTML();
 
         java2HTML.setJavaDirectorySource(Arrays.asList("./src/test/sample_java/testSource"));
@@ -17,5 +17,30 @@ public class Java2HTMLTest {
 
         java2HTML.buildJava2HTML();
 
+
+
+        String testJ2H = "-j .\\testSource -n The_Java2HTML_Source -t 4 -m 4";
+
+
+
     }
+
+    @Test
+    public void testSampleSourceWithJavaDoc() throws Exception {
+        String example3 = "-js %JAVA2HTML_HOME%\\src\\tests\\testSource -d ..\\..\\..\\JAVA_1_6_0_doc  -n Tests -m 4";
+    }
+
+    @Test
+    public void testThisSourceWithJavaDoc() throws Exception {
+        String test2JavaDocOnEnd = "-js C:\\work\\dev\\java2html\\src\\java -jd C:\\j2sdk1.4.0\\docs\\api -n The_Java2HTML_Source -t 4 -m 4";
+
+    }
+
+    @Test
+    public void testJava6Source() throws Exception {
+        String test2SDK = "j2h -js C:\\J2sdK1.4.0\\src";
+
+    }
+
+
 }
