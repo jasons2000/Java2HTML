69f8e05495108cc04ea5f306b3323ddff72af2e4
jasons_2000 <jasons_2000@05458adc-50a5-4456-917e-77870aaa2bcd>
2013-06-08 18:22:13 +0000

    fixed Mojo bug

--- java2html-maven-plugin/src/main/java/com/java2html/plugin/Java2HTMLMojo.java
+++ java2html-maven-plugin/src/main/java/com/java2html/plugin/Java2HTMLMojo.java
@@ -8,6 +8,8 @@ import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 
+import java.io.File;
+
 // http://maven.apache.org/plugin-tools/maven-plugin-tools-annotations/index.html
 
 /**
@@ -21,18 +23,20 @@ public class Java2HTMLMojo extends AbstractMojo {
 
     public void execute() throws MojoExecutionException {
 
-        getLog().info("Hello, world. X: " + project.getCompileSourceRoots() );
+        getLog().info("Hello, world. X: " + project.getCompileSourceRoots());
 
-        getLog().info("Hello, world. Y: " + project.getBuild().getOutputDirectory() );
+        getLog().info("Hello, world. Y: " + project.getBuild().getOutputDirectory());
 
-        Java2HTML java2HTML =new Java2HTML();
-        java2HTML.setJavaFileSource(project.getCompileSourceRoots());
-        java2HTML.setDestination(project.getBuild().getOutputDirectory());
+        Java2HTML java2HTML = new Java2HTML();
+
         try {
+            // todo have 1 set sourceMethod which can  work out dir/file/zips
+            java2HTML.setJavaDirectorySource(project.getCompileSourceRoots());
+            java2HTML.setDestination(project.getBuild().getDirectory() + File.separator + "java2html.output");
             java2HTML.buildJava2HTML();
         }
         catch (Exception e) {
-            throw new MojoExecutionException("Java2HTML encountered a problem" , e);
+            throw new MojoExecutionException("Java2HTML encountered a problem", e);
         }
 
 
